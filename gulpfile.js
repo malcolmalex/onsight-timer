cbe7ce5ded84600b0264900940b8187e4e901687
Malcolm Alexander <malcolm.alexander1@gmail.com>
2014-10-16 23:02:00 -0400

    Added clean & default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var downloadatomshell = require('gulp-download-atom-shell');
+var del = require('del');
 
-gulp.task('downloadatomshell', function(cb){
+// Download atom-shell local to the project
+gulp.task('downloadatomshell', function(cb) {
     downloadatomshell({
         version: '0.18.0',
         outputDir: 'build'
     }, cb);
 });
 
-gulp.task('demo', shell.task([
+// Default task does the build, dependent on having atom-shell in place
+// This is specific to MacOS
+// TODO: Generalize to other OSs
+gulp.task('default', ['downloadatomshell'], shell.task([
     'build/Atom.app/Contents/MacOS/Atom ./timer-app'
 ]));
+
+// Delete the build folder to clean
+gulp.task('clean', function(cb) {
+  del(['build'], cb);
+});
