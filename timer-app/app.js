fa464491b8019d0951b48072979134de28a23acb
Malcolm Alexander <malcolm.alexander1@gmail.com>
2014-10-18 03:39:09 -0400

    Drop tock.js, convert to RxJS
    Tock.js was standard call-back-based approach to timers and eventing,
    and it’s much less robust than RxJS. RxJS was needed for the chaining
    of the timers together and efficient management of the audio, ui, and
    timing events.

--- timer-app/app.js
+++ timer-app/app.js
@@ -1,29 +1,83 @@
-var timer = new Tock({
-  countdown: true,
-  interval: 10,
-  callback: function() {
-    var current_time = timer.msToTime(timer.lap());
-    $('#time').text(current_time.substring(0,5));
-  },
-  complete: function() {
-    $('#time').text("00:00");
-  }
-});
+//TODO: Save these to local storage eventually
+var Rx = require('./components/rxjs/dist/rx.lite.js');
+var numeral = require('./components/numeraljs/numeral.js');
 
+// Default transition and climb times (in seconds) for test
+var t_min = 3;
+var t_sec = 30;
+var c_min = 4;
+var c_sec = 0;
+
+// Calculate transition time and climb time from minutes and seconds
+var t_total_sec = t_min * 60 + t_sec;
+var c_total_sec = c_min * 60 + c_sec;
+
+// Declare subscriptions that will be udpated in various event handlers
+var mainSubscription;
+var transitionSubscription;
+var climbSubscription;
+
+// Setup audio files for notifying climbers of important times
+var BEGIN_CLIMBING = new Audio("audio/begin_climbing_edited.mp3");
+var BEGIN_TRANSITION = new Audio("audio/begin_transition_edited.mp3");
+var ONE_MINUTE = new Audio("audio/one_minute_edited.mp3");
+var TEN_SECONDS = new Audio("audio/ten_seconds_edited.mp3");
+var TIME_TIME = new Audio("audio/time_time_edited.mp3");
+
+// Create transition and climb streams with events 1000 ms apart, but
+// keep only transition time and climb time worth
+// Get the countdown by subtracting the seconds from the stream total
+var transitionTimer = Rx.Observable.timer(1000,1000)
+      .take(t_total_sec)
+      .map( function (x) {
+        return (t_total_sec - x);
+      });
+
+var climbTimer = Rx.Observable.timer(1000,1000)
+      .take(c_total_sec)
+      .map( function (x) {
+        return (c_total_sec - x);
+      });
+
+// Concatenate the streams together, and repeat. This enables each to be
+// treated independently, yet put together and repeated with easily
+// understood behavior.
+var mainTimer = Rx.Observable.concat(transitionTimer, climbTimer).repeat();
+
+// Open settings window
+function toggleDialog(transition) {
+  $('#paper-dialog[transition=' + transition + ']').toggle();
+}
+
+// Save transition and climb times in minutes and seconds
+function save() {
+  t_min = $('#t_min').val();
+  t_sec = $('#t_sec').val();
+  c_min = $('#c_min').val();
+  c_sec = $('#c_sec').val();
+}
+
+// Add event handlers
 $('#play').on('click', function() {
-  var start_time = timer.timeToMS("01:00");
-  timer.start(start_time);
+  console.log("started timer");
+
+  // start/subscribe to the main event stream
+  mainSubscription = mainTimer.subscribe(
+    function (x) {
+      var timeString = numeral(x).format('0:00:00').substr(2);
+      $('#time').text(timeString);
+      //console.log('Next: ' + numeral(x).format('0:00:00').substr(2));
+    },
+    function (err) {
+      console.log('Error: ' + err);
+    },
+    function () {
+      console.log('Completed');
+    }
+  );
 });
 
 $('#stop').on('click', function() {
-  timer.stop();
+  console.log("stoped timer");
+  mainSubscription.dispose();
 });
-
-$('#pause').on('click', function() {
-  timer.pause();
-});
-
-function toggleDialog(transition) {
-  var dialog = document.querySelector('paper-dialog[transition=' + transition + ']');
-  dialog.toggle();
-}
